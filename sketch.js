00c5f18f2d197e0bfa4aafb34d9365cb7914b32a
rrtg123 <raghavhrathi@gmail.com>
2021-01-13 15:56:51 +0530

    I like multiplayer

--- sketch.js
+++ sketch.js
@@ -1,24 +1,31 @@
 var ball;
+var database,position;
 
 function setup(){
-    createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
+    createCanvas(500,500); 
+
+    database=firebase.database();
+    //loc node is containing the reference to ball /positions in firebase
+    var locnode=database.ref("ball/positions");
+    locnode.on("value",readop,showerror);
+    //on is actually a listener which is actually listening to the values of database
+    ball = createSprite(250,250,10,10); 
     ball.shapeColor = "red";
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writepos(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writepos(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writepos(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writepos(0,+1);
     }
     drawSprites();
 }
@@ -26,4 +33,21 @@ function draw(){
 function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
+}
+//sdk=software development kit
+function readop(data){
+    position=data.val()
+    ball.x=position.x
+    ball.y=position.y
+}
+
+function showerror(){
+    console.log("error")
+}
+function writepos(x,y){
+database.ref("ball/positions").set({
+x:ball.x+x,
+y:ball.y+y
+})
+
 }
